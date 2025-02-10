54610734dc2f2548578395fc383c7c1e5239a9be
verena <9377970+vpchung@users.noreply.github.com>
2025-02-10 14:21:08 -0800

    update query

--- toolkit/queries.py
+++ toolkit/queries.py
@@ -1,23 +1,27 @@
-def query_users_trend():
+def query_challenges():
     """Return the total number of newly-created user accounts in Synapse for each year."""
 
     return """
-    WITH yearly_new_users AS (
+    WITH syn_users AS (
         SELECT
-            year(change_timestamp) AS query_year,
-            count(DISTINCT id) AS yearly_new_users
-        FROM 
+            id,
+            user_name
+        FROM
             synapse_data_warehouse.synapse.userprofile_latest
-        WHERE
-            change_type = 'CREATE'
-        GROUP BY
-            year(change_timestamp)
     )
     SELECT
-        query_year,
-        sum(yearly_new_users) OVER (ORDER BY query_year) AS CUMULATIVE_USERS
+        DATE(created_on) as date,
+        user_name AS project_creator,
+        name,
+        project_id
     FROM
-        yearly_new_users
-    ORDER BY
-        query_year DESC;
+        synapse_data_warehouse.synapse.node_latest
+    LEFT JOIN
+        syn_users
+    ON syn_users.id = created_by
+    WHERE
+        node_type = 'project'
+        AND name LIKE ANY('%Challenge', 'BraTS ____' ,'FeTS% ____')
+        AND is_public
+    ORDER BY date DESC
     """
