0319b35ffd7f15d294eed848341ab54fde38aae8
verena <9377970+vpchung@users.noreply.github.com>
2025-02-10 14:21:34 -0800

    add two util functions for readable text display

--- toolkit/utils.py
+++ toolkit/utils.py
@@ -13,3 +13,17 @@ def get_data_from_snowflake(query=""):
     session = connect_to_snowflake()
     node_latest = session.sql(query).to_pandas()
     return node_latest
+
+
+def human_format(num: int) -> str:
+    """Convert number to more human-readable format."""
+    num = float('{:.3g}'.format(num))
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return "{}{}".format("{:f}".format(num).rstrip(r"\.0"), ["", "k", "m", "b"][magnitude])
+
+
+def force_display_all_rows(df) -> int:
+    return 35 * len(df) + 38
