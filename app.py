b10569ac3ff75bf4dc9172b559231d33698a617d
verena <9377970+vpchung@users.noreply.github.com>
2025-02-19 17:53:54 -0800

    add query for getting download counts; show new page option

--- app.py
+++ app.py
@@ -107,6 +107,40 @@ def app(selected_option):
             }
         )
 
+    if selected_option == "Data Downloads":
+        st.markdown("Retrieve the number of challenge data downloads by challenge.")
+        syn_id = st.text_input(
+            "**Challenge Synapse ID**",
+            max_chars=11,
+            placeholder="syn12345678",
+            help=(
+                "The Challenge Synapse ID is the same as the Project SynID, "
+                "which can be found in the top-left corner of the Project."
+            ),
+        )
+
+        if syn_id:
+            try:
+                download_counts = get_data_from_snowflake(
+                    query_data_download_counts(syn_id)
+                )
+                download_counts["NODE_ID"] = "syn" + download_counts["NODE_ID"].astype(str)
+                st.dataframe(
+                    download_counts,
+                    hide_index=True,
+                    use_container_width=True,
+                    height=1000,
+                    column_config={
+                        "NODE_ID": st.column_config.TextColumn("Synapse ID", width="small"),
+                        "FILENAME": st.column_config.TextColumn("Filename"),
+                        "NUMBER_OF_DOWNLOADS": st.column_config.TextColumn(
+                            "Download Count", width="small"
+                        ),
+                    },
+                )
+            except Exception:
+                st.markdown(f":red[{syn_id} is not a valid ID, please try again.]")
+
     # 4. Basic footer
     # -------------------------------------------------------------------------
     st.markdown(footer_html(), unsafe_allow_html=True)
--- toolkit/queries.py
+++ toolkit/queries.py
@@ -1,3 +1,5 @@
+import re
+
 def query_challenges():
     """Return the total number of newly-created user accounts in Synapse for each year."""
 
@@ -24,4 +26,57 @@ def query_challenges():
         AND name LIKE ANY('%Challenge', 'BraTS ____' ,'FeTS% ____')
         AND is_public
     ORDER BY date DESC
+    """
+
+
+def query_data_download_counts(syn_id):
+    """Return the number of data downloads for a given challenge."""
+
+    syn_id = re.search(r"^[a-z:]*(\d{8})$", syn_id).group(1)
+    return f"""
+    WITH challenge_files AS (
+        SELECT
+            id AS node_id,
+            name as filename,
+            file_handle_id
+        FROM
+            synapse_data_warehouse.synapse.node_latest
+        WHERE
+            project_id = {syn_id} AND
+            node_type = 'file' AND
+            name NOT LIKE '%_logs.zip' AND
+            name NOT LIKE '%_docker.log' AND
+            name NOT LIKE '%predictions.%' AND
+            name NOT LIKE 'mlcube.yaml' AND
+            name NOT LIKE 'parameters.yaml' AND
+            name NOT LIKE 'additional_files.tar.gz' AND
+            name NOT LIKE '%scores.csv'
+    ), dedup_download_records AS (
+        SELECT
+            DISTINCT user_id, file_handle_id, record_date
+        FROM
+            synapse_data_warehouse.synapse.filedownload
+        WHERE
+            project_id = {syn_id}
+    ), downloads AS (
+        SELECT
+            *
+        FROM
+            challenge_files
+        LEFT JOIN
+            dedup_download_records
+        ON
+            challenge_files.file_handle_id = dedup_download_records.file_handle_id
+    )
+
+    SELECT
+        node_id,
+        MAX(filename) AS filename,
+        COUNT(record_date) AS number_of_downloads
+    FROM
+        downloads
+    GROUP BY
+        node_id
+    ORDER BY
+        number_of_downloads DESC;
     """
